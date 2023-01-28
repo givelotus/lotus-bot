4f86c4e0e875c9e6534e807c801c2aea8d2af05d
maff1989 <matthew@urgero.net>
2023-01-28 15:50:14 -0600

    Discord adjustments (#15)
    * Many formatting changes, several optimizations
    
    In summary:
    - Convert many try/catch handlers to throw `Error`s rather than log
    to console
    - Adjust `setup()` and `launch()` logic; remove `this.rest` and use
    `this.client.rest` instead so that we can pass `apiKey` without needing
    to instantiate a private class property
    - Move the `this.client.on('ready')` handler to a private class method
    - Add new `_setRandomActivity()` method which is called during
    `_handleReady()` to set the bot's current activity. Activities are
    stored in a class property `activities: string[]` and the available
    activity strings are pushed during `setup()`.
    - Many more formatting fixes
    
    * fix Client apiKey + clearInterval on shutdown
    
    Previous commit introduced bug where the Client would not properly
    authenticate with Discord without having `this.client.token` set with
    the `apiKey`. This commit fixes that.
    
    Another bug was noticed where the `setInterval()` for updating the bot
    activity was not being cleared on shutdown, causing the node.js process
    to hang until force-killed. This commit adds the `clearInterval()` to
    the `stop()` method to resolve this.
    
    ---------
    
    Co-authored-by: maff1989 <maff1989@gmail.com>

--- lib/platforms/discord.ts
+++ lib/platforms/discord.ts
@@ -16,158 +16,155 @@ import { format } from 'node:util';
 import { Platform } from '.';
 import config from '../../config';
 
-//Branding
+// DM Branding
 const primaryColor: ColorResolvable = 0xa02fe4;
 const secondaryColor: ColorResolvable = 0xf0409b;
 
 export type DiscordMessage = ChatInputCommandInteraction;
 
+type Command = {
+  name: string,
+  description: string,
+  options?: CommandOption[]
+};
+
+type CommandOption = {
+  type: number,
+  name: string,
+  description: string,
+  required: boolean
+};
+
 export declare interface Discord {
-    on(event: 'Balance', callback: (
-      platformId: string
-    ) => void): this;
-    on(event: 'Give', callback: (
-      chatId: string,
-      replyToMessageId: number,
-      fromId: string,
-      fromUsername: string,
-      toId: string,
-      toUsername: string,
-      value: string
-    ) => void): this;
-    on(event: 'Deposit', callback: (
-      platformId: string,
-    ) => void): this;
-    on(event: 'Withdraw', callback: (
-      platformId: string,
-      wAmount: number,
-      wAddress: string,
-    ) => void): this;
-  }
+  on(event: 'Balance', callback: (
+    platformId: string
+  ) => void): this;
+  on(event: 'Give', callback: (
+    chatId: string,
+    replyToMessageId: number,
+    fromId: string,
+    fromUsername: string,
+    toId: string,
+    toUsername: string,
+    value: string
+  ) => void): this;
+  on(event: 'Deposit', callback: (
+    platformId: string,
+  ) => void): this;
+  on(event: 'Withdraw', callback: (
+    platformId: string,
+    wAmount: number,
+    wAddress: string,
+  ) => void): this;
+}
 
 export class Discord 
 extends EventEmitter
 implements Platform {
-    private lastReplyTime: number;
-    private discordToken: string;
-    private clientId: string;
-    private guildId: string;
-    private client: Client;
-    private rest: REST;
-    private commands = [];
-    constructor() {   
-        super();
-        this.lastReplyTime = Date.now();
-        // Discord bot client and api setup
-        this.clientId = config.discord.clientId;
-        this.guildId = config.discord.guildId;
-        this.client = new Client({
-          intents: [GatewayIntentBits.Guilds,GatewayIntentBits.DirectMessages],
-          partials: [Partials.Channel]
-        });
-      }
+  private lastReplyTime: number;
+  private clientId: string;
+  private guildId: string;
+  private client: Client;
+  private commands: Command[] = [];
+  private activities: string[] = [];
+  private activityInterval: NodeJS.Timer;
+
+  constructor() {   
+    super();
+    this.lastReplyTime = Date.now();
+    // Discord bot client and api setup
+    this.clientId = config.discord.clientId;
+    this.guildId = config.discord.guildId;
+    this.client = new Client({
+      intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages],
+      partials: [Partials.Channel]
+    });
+    this.activities.push(
+      "🪷 Give appreciation with Lotus 🪷",
+      "🪷 givelotus.org 🪷",
+      "🪷 Use /balance to start 🪷"
+    );
+  };
   /**
    * Instantiate the bot with API key. Also set up event handlers.
    * @param key - API key, as String
    */
   setup = async (apiKey: string) => {
-    try{
-        this.discordToken = apiKey;
-        this.rest = new REST({ version: '10' }).setToken(this.discordToken);
-        //Command JSON for Discord Command Registration Type 10 is number, 3 is string?
-        this.commands = [
+    //Command JSON for Discord Command Registration Type 10 is number, 3 is string?
+    this.commands = [
+      {
+        name: 'give',
+        description: 'Give XPI to another user.',
+        options: [
           {
-            name: 'give',
-            description: 'Give XPI to another user.',
-            options:[
-              {
-                type:6,
-                name:"to",
-                description:"User to give XPI to",
-                required:true
-              },
-              {
-                type:10,
-                name:"amount",
-                description:"Amount of XPI to give.",
-                required:true
-              }
-            ]
+            type: 6,
+            name: "to",
+            description: "User to give XPI to",
+            required: true
           },
           {
-            name: 'balance',
-            description: 'Get balance information for the currently logged in user.'
+            type: 10,
+            name: "amount",
+            description: "Amount of XPI to give.",
+            required: true
+          }
+        ]
+      },
+      {
+        name: 'balance',
+        description: 'Get balance information for the currently logged in user.'
+      },
+      {
+        name: 'deposit',
+        description: 'Deposit XPI into your wallet in the bot.'
+      },
+      {
+        name: 'withdraw',
+        description: 'Withdraw XPI from your wallet in the bot.',
+        options: [
+          {
+            type: 10,
+            name: "amount",
+            description: "Amount of XPI to withdraw.",
+            required: true
           },
           {
-            name: 'deposit',
-            description: 'Deposit XPI into your wallet in the bot.'
-          },
-          {
-            name: 'withdraw',
-            description: 'Withdraw XPI from your wallet in the bot.',
-            options:[
-              {
-                type:10,
-                name:"amount",
-                description:"Amount of XPI to withdraw.",
-                required:true
-              },
-              {
-                type:3,
-                name:"address",
-                description:"XPI Address for your external wallet.",
-                required:true
-              }
-            ]
-          },
-          {
-            name: 'ping',
-            description: 'pong'
-          },
-          {
-            name: 'ilovelotus',
-            description: 'I 💖 🪷'
-          },
-        ];
-    }catch(err){
-        throw new Error(`SETUP: ${err}`);
+            type: 3,
+            name: "address",
+            description: "XPI Address for your external wallet.",
+            required: true
+          }
+        ]
+      },
+      {
+        name: 'ping',
+        description: 'pong'
+      },
+      {
+        name: 'ilovelotus',
+        description: 'I 💖 🪷'
+      },
+    ];
+    try {
+      this.client.on('ready', this._handleReady);
+      this.client.on('messageCreate', this._handleDirectMessage);
+      this.client.on('interactionCreate', this._handleCommandMessage);
+      this.client.token = apiKey;
+      this.client.rest = new REST({ version: '10' }).setToken(apiKey);
+    } catch (e: any) {
+      throw new Error(`setup: ${e.message}`);
     }
 
   };
   /** Activate the bot */
   launch = async () => {
     await this._registerCommands();
-
-    this.client.on('ready', () => {
-        console.log(`Logged in as ${this.client.user.tag}!`);
-        this.client.user.setActivity(
-          "🪷 Give appreciation with Lotus 🪷",
-          {type: ActivityType.Playing}
-        );
-        const activities = [
-          "🪷 Give appreciation with Lotus 🪷",
-          "🪷 givelotus.org 🪷",
-          "🪷 Use /balance to start 🪷"
-        ];
-        setInterval(() => {
-          // generate random number between 1 and list length.
-          const randomIndex = Math.floor(Math.random() * (activities.length - 1) + 1);
-          const newActivity = activities[randomIndex];
-          this.client.user.setActivity(newActivity, {type: ActivityType.Playing});
-        }, 10000);
-        
-    });
-    // Handle Direct Messages and Commands from Server(s)
-    this.client.on('messageCreate', this._handleDirectMessage);
-    this.client.on('interactionCreate', this._handleCommandMessage);
-
-    // Connect to Discord
-    this.client.login(this.discordToken);
+    await this.client.login();
   };
   /** Deactivate the bot */
   stop = async () => {
-    console.log("Bye daddy. uwu");
-    await this.client.destroy();
+    clearInterval(this.activityInterval);
+    this.client.destroy();
   };
   getBotId = () => this.clientId;
   sendBalanceReply = async (
@@ -175,68 +172,75 @@ implements Platform {
     balance: string,
     interaction: ChatInputCommandInteraction
   ) => {
-    try{
-      await interaction.reply({ content: format(BOT.MESSAGE.BALANCE, balance), ephemeral: true });
-    } catch(err){
-      console.log(err.message);
+    try {
+      await interaction.reply({
+        content: format(BOT.MESSAGE.BALANCE, balance),
+        ephemeral: true
+      });
+    } catch (e: any) {
+      throw new Error(`sendBalanceReply: ${e.message}`);
     }
-  }
+  };
   sendDepositReply = async (platformId: string, address: string) => {
-    try{
+    try {
       const depositReplyEmbed = new EmbedBuilder()
         .setColor(primaryColor)
         .setTitle(`View address on the Explorer`)
         .setURL(`${config.wallet.explorerUrl}/address/${address}`)
         .setDescription('Send Lotus here to fund your account')
-        .addFields(
-          { name: 'Lotus Address', value: address }
-        )
+        .addFields({ name: 'Lotus Address', value: address })
         .setImage(`${config.wallet.explorerUrl}/qr/${address}`)
         .setTimestamp();
 
       const userObj = await this.client.users.fetch(platformId);
       await userObj.send({embeds: [depositReplyEmbed]});
-    }catch(err){
-      console.log(err.message);
+    } catch (e: any) {
+      throw new Error(`sendDepositReply: ${e.message}`);
     }
-  }
+  };
   sendDepositReceived = async (
     platformId: string,
     txid: string,
     amount: string
   ) => {
-    try{
-      const despositRecEmbed = new EmbedBuilder()
+    try {
+      const embedMessage = new EmbedBuilder()
         .setColor(primaryColor)
         .setTitle('Deposit Received 🪷 - Click Here to see the tx.')
         .setURL(`${config.wallet.explorerUrl}/tx/${txid}`)
-        .setDescription(`I received your deposit of ${amount} XPI. I will let you know when it confirms.`)
+        .setDescription(
+          `I received your deposit of ${amount} XPI. ` +
+          `I will let you know when it confirms.`
+        )
         .setTimestamp();
 
-    const userObj = await this.client.users.fetch(platformId);
-    await userObj.send({embeds: [despositRecEmbed]});
-    }catch(err){
-      console.log(err.message);
+      const userObj = await this.client.users.fetch(platformId);
+      await userObj.send({ embeds: [embedMessage] });
+    } catch (e: any) {
+      throw new Error(`sendDepositReceived: ${e.message}`);
     }
-  }
+  };
   sendDepositConfirmed = async (
     platformId: string,
     txid: string,
     amount: string,
     balance: string
   ) => {
-    try{
-      const despositRecEmbed = new EmbedBuilder()
+    try {
+      const embedMessage = new EmbedBuilder()
         .setColor(secondaryColor)
         .setTitle('Deposit Confirmed 🪷 - Click Here to see the tx.')
         .setURL(`${config.wallet.explorerUrl}/tx/${txid}`)
-        .setDescription(`Your deposit of ${amount} XPI has been confirmed! Your balance is now ${balance} XPI.`)
+        .setDescription(
+          `Your deposit of ${amount} XPI has been confirmed! ` +
+          `Your balance is now ${balance} XPI.`
+        )
         .setTimestamp();
 
-    const userObj = await this.client.users.fetch(platformId);
-    await userObj.send({embeds: [despositRecEmbed]});
-    }catch(err){
-      console.log(err.message);
+      const userObj = await this.client.users.fetch(platformId);
+      await userObj.send({ embeds: [embedMessage] });
+    } catch (e: any) {
+      throw new Error(`sendDepositConfirmed: ${e.message}`);
     }
   };
   sendGiveReply = async (
@@ -247,17 +251,17 @@ implements Platform {
     amount: string,
     interaction: ChatInputCommandInteraction
   ) => {
-    try{
+    try {
+      const { user, options } = interaction;
+      const fromUser = `<@${user.id}>`;
+      const toUser = `<@${options.getUser('to').id}>`;
       await interaction.reply(
-        format(BOT.MESSAGE.GIVE,
-          "<@"+interaction.user.id+">",
-          amount,
-          "<@"+interaction.options.getUser('to').id+">"
-        ));
-    }catch(err){
-      console.log(err.message);
+        format(BOT.MESSAGE.GIVE, fromUser, amount, toUser)
+      );
+    } catch (e: any) {
+      throw new Error(`sendGiveReply: ${e.message}`);
     }
-  }
+  };
   sendWithdrawReply = async (
     platformId: string,
     {
@@ -270,105 +274,121 @@ implements Platform {
       error?: string
     }
   ) => {
-    try{
+    try {
       const userObj = await this.client.users.fetch(platformId);
-      if(error){
-        await userObj.send(`There was an error processing your withdrawal: ${error}`);
-      } else {
-        const despositRecEmbed = new EmbedBuilder()
-          .setColor(primaryColor)
-          .setTitle('Withdrawal Successful 🪷 - Click Here to see the tx.')
-          .setURL(`${config.wallet.explorerUrl}/tx/${txid}`)
-          .setDescription(`Your withdrawal of ${amount} XPI was successful!`)
-          .setTimestamp();
-
-        await userObj.send({embeds: [despositRecEmbed]});
+      if (error) {
+        await userObj.send(format(BOT.MESSAGE.WITHDRAW_FAIL, error));
+        return;
       }
-    }catch(err){
-      console.log(err.message);
+      const embedMessage = new EmbedBuilder()
+        .setColor(primaryColor)
+        .setTitle('Withdrawal Successful 🪷 - Click Here to see the tx.')
+        .setURL(`${config.wallet.explorerUrl}/tx/${txid}`)
+        .setDescription(`Your withdrawal of ${amount} XPI was successful!`)
+        .setTimestamp();
+
+      await userObj.send({embeds: [embedMessage]});
+    } catch (e: any) {
+      throw new Error(`sendWithdrawReply: ${e.message}`);
     }
-  }
+  };
   private _registerCommands = async () => {
     try {
-        console.log(`Started refreshing ${this.commands.length} application (/) commands.`);
-        // The put method is used to fully refresh all commands in the guild with the current set
-        await this.rest.put(
-            Routes.applicationGuildCommands(this.clientId, this.guildId),
-            { body: this.commands },
-        );
-        console.log(`Finshed refreshing ${this.commands.length} application (/) commands.`);
-    } catch (err) {
+      await this.client.rest.put(
+        Routes.applicationGuildCommands(this.clientId, this.guildId),
+        { body: this.commands },
+      );
+    } catch (e: any) {
         // And of course, make sure you catch and log any errors!
-        throw new Error(`REGISTERCOMMANDS: ${err.message}`);
+        throw new Error(`_registerCommands: ${e.message}`);
     }
-  }
+  };
+  private _handleReady = () => {
+    console.log(`Logged in as ${this.client.user.tag}!`);
+    this._setRandomActivity();
+    this.activityInterval = setInterval(this._setRandomActivity, 10000);
+  };
   private _handleDirectMessage = async (message: Message) => {
     const {
       author,
       content
     } = message;
 
-    if(author.id == this.clientId) return;
+    if (author.id == this.clientId) {
+      return;
+    }
 
-    const dmCommand = content.trim().split(" ");
-    switch(dmCommand[0]){
+    const words = content.trim().split(" ");
+    const command = words[0];
+    const amount = Number(words[1]);
+    const wAddress = words[2] || null;
+    switch (command) {
       case "balance":
-        this.emit('Balance',author.id, message);
+        this.emit('Balance', author.id, message);
         break;
       case "deposit":
-        this.emit('Deposit',author.id);
+        this.emit('Deposit', author.id);
         break;
       case "withdraw":
-        if(dmCommand.length < 3){
-          await message.reply("You must use the following syntax for withdrawing:\r\n`withdraw <amount> <external_address>`");
+        if (words.length < 3) {
+          await message.reply(
+            `You must use the following syntax for withdrawing:\r\n` +
+            "`withdraw <amount> <external_address>`"
+          );
           break;
         }
-        if(parseInt(dmCommand[1]) <= 0){
+        if (isNaN(amount) || amount <= 0) {
           await message.reply("The value for withdrawal must be greater than 0.");
           break;
         }
-        this.emit('Withdraw',author.id,dmCommand[1],dmCommand[2]);
+        this.emit('Withdraw', author.id, amount, wAddress);
         break;
       default:
-        message.reply(`You can only use the following verbs in my DMs:
-        
-**balance** - Get your current balance in the bot.
-**deposit** - Get the deposit information needed to send XPI to your bot wallet.
-**Withdraw** - Withdraw XPI from your bot wallet to an external wallet address.
-
-To use the Withdraw command, please use the following syntax:
-\`withdraw <amount> <external_address>\`
-        `);
+        message.reply(
+          `You can only use the following verbs in my DMs:\r\n\r\n` +
+          `**balance** - Get your current balance in the bot.\r\n` +
+          `**deposit** - Get the address needed to deposit XPI.\r\n` +
+          `**Withdraw** - Withdraw XPI to an external wallet.\r\n\r\n` +
+          "Withdraw command syntax: `withdraw <amount> <external_address>`"
+        );
         break;
     }
   }
-  private _handleCommandMessage = async (interaction: ChatInputCommandInteraction) =>{
-    if (!interaction.isChatInputCommand()) return;
+  private _handleCommandMessage = async (
+    interaction: ChatInputCommandInteraction
+  ) => {
+    if (!interaction.isChatInputCommand()) {
+      return;
+    }
     const {
       user,
       channelId,
       options,
       commandName
     } = interaction;
-    const fromUser = user.username+"#"+user.discriminator;
+    const fromUser = `${user.username}#${user.discriminator}`;
     const platformId = user.id;
-    console.log(`Command sent from ${fromUser} on channel ${this.guildId}:${channelId} = ${commandName}`);
+    console.log(
+      `Command sent from ${fromUser} on channel ` +
+      `${this.guildId}:${channelId} = ${commandName}`
+    );
     const xpiAmount = options.getNumber('amount') ?? 0;
 
-    switch(commandName){
+    try {
+      switch (commandName) {
         case "give":
           //Process give shit.
           const to = options.getUser('to');
           const toId = to.id
-          const toUsername = to.username+"#"+to.discriminator;
-          if(xpiAmount <= 0){
+          const toUsername = `${to.username}#${to.discriminator}`;
+          if (xpiAmount <= 0) {
             await interaction.reply({
               content: format(BOT.MESSAGE.ERR_AMOUNT_INVALID, xpiAmount),
               ephemeral: true
             });
             break;
           }
-          if(this.clientId == to.id){
+          if (this.clientId == to.id) {
             await interaction.reply({
               content: format(BOT.MESSAGE.ERR_GIVE_TO_BOT),
               ephemeral: true
@@ -390,7 +410,7 @@ To use the Withdraw command, please use the following syntax:
           this.emit('Balance', platformId, interaction);
           break;
         case "deposit":
-          this.emit('Deposit',platformId);
+          this.emit('Deposit', platformId);
           await interaction.reply({
             content: `Please check your DMs for reply message.`,
             ephemeral: true
@@ -420,6 +440,19 @@ To use the Withdraw command, please use the following syntax:
             ephemeral: true
           });
           break;
+      }
+    } catch (e: any) {
+      throw new Error(`_handleCommandMessage: ${e.message}`);
     }
-  }
-}
+  };
+
+  private _setRandomActivity = () => {
+    const randomIndex = Math.floor(
+      Math.random() * (this.activities.length - 1) + 1
+    );
+    this.client.user.setActivity(
+      this.activities[randomIndex],
+      { type: ActivityType.Playing }
+    );
+  };
+};
