e0ed4c27ad92759a4ab4d3a239f30d657ead769d
maff1989 <matthew@urgero.net>
2023-06-15 09:32:03 -0500

    scripts/fortunes.ts: small tweaks/optimizations
    - Set up `main` async function instead of calling anonymous function
    - Use `fs/promises` for filesystem operations (i.e. async/await)
    - Adjust some `const` names

--- scripts/fortunes.ts
+++ scripts/fortunes.ts
@@ -1,44 +1,44 @@
 import { Prisma, PrismaClient } from "../prisma/prisma-client-js";
 import { Configuration, OpenAIApi } from "openai";
-import * as fs from "fs";
+import * as fs from "fs/promises";
 import * as path from "path";
 import config from '../config';
 import { newUUID } from "../util";
 
 const prisma = new PrismaClient();
 const aiConfig = new Configuration({
-  organization:config.openai.org,
+  organization: config.openai.org,
   apiKey: config.openai.key
 });
 const openai = new OpenAIApi(aiConfig);
 
-(async () => {
+const main = async () => {
   try {
     const promptsPath = path.join(process.cwd(), 'scripts', "fortune-prompts");
-    const promptFiles = fs.readdirSync(promptsPath);
+    const promptFiles = await fs.readdir(promptsPath);
     const index = Math.round(Math.random() * (promptFiles.length - 1));
     const promptCategory = promptFiles[index];
     const filePath = path.join(promptsPath, promptCategory);
-    const prompt = fs.readFileSync(filePath).toString();
+    const promptBuf = await fs.readFile(filePath);
     // Generate and submit the request using provided prompt
     console.log(`INFO: Sending request to OpenAI for fortunes in category ${promptCategory}`);
-    const aiRequest = await openai.createChatCompletion({
+    const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       user: "lotus-b-fortune",
       temperature: 0.7,
       messages: [{
         role: "user",
-        content: prompt,
+        content: promptBuf.toString(),
         name: "lotus"
       }],
     });
     // Get the AI response message
     console.log(`INFO: Processing response from OpenAI`);
-    const aires = aiRequest.data.choices[0].message?.content;
-    if (!aires) {
+    const json = response.data.choices[0].message?.content;
+    if (!json) {
       throw new Error('No response from API');
     }
-    const fortuneArray: string[] = JSON.parse(aires);
+    const fortuneArray: string[] = JSON.parse(json);
     const fortuneInserts: Prisma.FortuneCreateInput[] = [];
     fortuneArray.map(fortuneString => fortuneInserts.push({
       id: newUUID(),
@@ -66,5 +66,6 @@ const openai = new OpenAIApi(aiConfig);
   } finally {
     await prisma.$disconnect();
   }
-})();
+};
 
+main();
