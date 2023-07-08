ba014751fe607f79fb20750aabf584b437815789
maff1989 <matthew@urgero.net>
2023-07-08 00:19:38 -0500

    Finalize `handler.ts` module
    Finish migrating from `lotusbot.ts` module and structuring handler
    functions for use with platform modules.

--- lib/handler.ts
+++ lib/handler.ts
@@ -1,11 +1,5 @@
-import {
-  Platforms,
-  PlatformName,
-  PlatformMessage,
-  Platform,
-} from './platforms';
+import { PlatformName } from './platforms';
 import * as Util from '../util';
-import config from '../config';
 import { TRANSACTION } from '../util/constants';
 import {
   AccountUtxo,
@@ -38,45 +32,56 @@ export class Handler extends EventEmitter {
     super();
     this.prisma = prisma;
     this.wallets = wallets;
+    // Set up event handlers once we are ready
+    this.wallets.on('AddedToMempool', this.walletsUtxoAddedToMempool);
   };
   /** Informational and error logging */
   log = (
     module: string,
     message: string
   ) => console.log(`${module.toUpperCase()}: ${message}`);
-  /** Platform notification error logging */
-  logPlatformNotifyError = (
-    platform: PlatformName,
-    msg: string,
-    error: string
-  ) => this.log(platform, `${msg} failed to notify user: ${error}`);
   /* Called by any bot module that runs into unrecoverable error */
-  shutdown = () => this.emit('shutdown');
-
+  shutdown = () => this.emit('Shutdown');
+  /** Make sure we process deposits we received while offline */
+  init = async () => {
+    this.log(MAIN, `reconciling deposits with UTXO set`);
+    try {
+      const utxos = this.wallets.getUtxos();
+      const deposits = await this.prisma.getDeposits();
+      const newDeposits = utxos.filter(u => {
+        return deposits.findIndex(d => u.txid == d.txid) < 0;
+      });
+      for (const deposit of newDeposits) {
+        await this._saveDeposit(deposit);
+      }
+    } catch (e: any) {
+      throw new Error(`_initReconcileDeposits: ${e.message}`);
+    }
+  };
+  /**  */
   walletsUtxoAddedToMempool = async (
     utxo: AccountUtxo
   ) => {
     try {
       await this._saveDeposit(utxo);
     } catch (e: any) {
-      this.log(MAIN, `FATAL: walletsUtxoAddedToMempool: ${e.message}`);
-      this.emit('shutdown');
+      this.log(MAIN, `walletsUtxoAddedToMempool: FATAL: ${e.message}`);
+      this.shutdown();
     }
   };
 
   processBalanceCommand = async (
     platform: PlatformName,
     platformId: string,
-  ): Promise<number> => {
-    this.log(platform, `${platformId}: balance command received`);
-    const msg = `${platformId}: balance: `;
+  ): Promise<string> => {
+    const msg = `${platformId}: balance`;
+    this.log(platform, `${msg}: command received`);
     try {
       const { accountId } = await this._getIds(platform, platformId);
       const balance = await this.wallets.getAccountBalance(accountId);
-      return balance;
+      return Util.toXPI(balance);
     } catch (e: any) {
-      this.log(MAIN, `FATAL: plaformBalanceCommand: ${e.message}`);
-      this.emit('shutdown');
+      throw new Error(`${msg}: ERROR: ${e.message}`);
     }
   };
   
@@ -84,15 +89,13 @@ export class Handler extends EventEmitter {
     platform: PlatformName,
     platformId: string,
   ) => {
-    this.log(platform, `${platformId}: deposit command received`);
-    const msg = `${platformId}: deposit: `;
+    const msg = `${platformId}: deposit`;
+    this.log(platform, `${msg}: command received`);
     try {
       const { userId } = await this._getIds(platform, platformId);
-      const address = this.wallets.getXAddress(userId);
-      return address;
+      return this.wallets.getXAddress(userId);
     } catch (e: any) {
-      this.log(MAIN, `FATAL: platformDepositCommand: ${e.message}`);
-      this.emit('shutdown');
+      throw new Error(`${msg}: ERROR: ${e.message}`);
     }
   };
 
@@ -104,20 +107,13 @@ export class Handler extends EventEmitter {
     toUsername: string,
     value: string,
   ) => {
-    this.log(
-      platform,
-      `${fromId}: give command received: ${fromUsername} -> ${toUsername}`
-    );
     const sats = Util.toSats(value);
     const msg =
-      `${fromId}: give: ${fromUsername} -> ${toId} (${toUsername}): ${sats} sats: `;
+      `${fromId}: give: ${fromUsername} -> ${toId} (${toUsername}): ${sats} sats`;
+    this.log(platform, `${msg}: command received`);
     try {
       if (sats < MIN_OUTPUT_AMOUNT) {
-        this.log(
-          platform,
-          msg + `minimum required: ${MIN_OUTPUT_AMOUNT}`
-        );
-        return;
+        throw new Error(`${msg}: ERROR: minimum required: ${MIN_OUTPUT_AMOUNT}`);
       }
       // Create account for fromId if not exist
       const {
@@ -126,8 +122,7 @@ export class Handler extends EventEmitter {
       } = await this._getIds(platform, fromId);
       const balance = await this.wallets.getAccountBalance(fromAccountId);
       if (sats > balance) {
-        this.log(platform, msg + `insufficient balance: ${balance}`);
-        return;
+        throw new Error(`${msg}: ERROR: insufficient balance: ${balance}`);
       }
       // Create account for toId if not exist
       const {
@@ -140,78 +135,53 @@ export class Handler extends EventEmitter {
         sats
       });
       // save give to database before broadcasting
-      await this.prisma.saveGive({
-        txid: tx.txid,
-        platform: platform.toLowerCase(),
-        timestamp: new Date(),
-        fromId: fromUserId,
-        toId: toUserId,
-        value: sats.toString()
-      });
-      this.log(DB, msg + `saved to db: ` + tx.txid);
+      try {
+        await this.prisma.saveGive({
+          txid: tx.txid,
+          platform: platform.toLowerCase(),
+          timestamp: new Date(),
+          fromId: fromUserId,
+          toId: toUserId,
+          value: sats.toString()
+        });
+      } catch (e: any) {
+        throw new Error(`${msg}: failed to save give: ${e.message}`);
+      }
+      this.log(DB, `${msg}: saved to db: ${tx.txid}`);
       // try to broadcast the give tx
       try {
         await this.wallets.broadcastTx(tx);
       } catch (e: any) {
-        this.log(platform, msg + `broadcast failed: ${e.message}`);
         await this.prisma.deleteGive(tx.txid);
-        return;
+        throw new Error(`${msg}: ERROR: broadcast failed: ${e.message}`);
       }
       // return broadcasted tx data
       return {
         txid: tx.txid,
         amount: Util.toXPI(tx.outputs[0].satoshis) 
       };
-      /*
-      try {
-        // Send Give success reply to chat
-        await bot.sendGiveReply(
-          chatId,
-          replyToMessageId,
-          fromUsername,
-          toUsername,
-          tx.txid,
-          Util.toXPI(sats),
-          message
-        );
-        this.log(platform, msg + `success: user notified`);
-      } catch (e: any) {
-        this.logPlatformNotifyError(platform, msg, e.message);
-      }
-      */
     } catch (e: any) {
-      this.log(MAIN, `FATAL: platformGiveCommand: ${e.message}`);
-      this.emit('shutdown');
+      throw new Error(e.message);
     }
   };
 
   processWithdrawCommand = async (
     platform: PlatformName,
     platformId: string,
-    outAmount: number,
+    outAmount: string,
     outAddress: string,
   ): Promise<{
-    txid?: string,
-    amount?: string,
-    error?: string,
-  }> => {
-    this.log(
-      platform,
-      `${platformId}: withdraw command received: ${outAmount} ${outAddress}`
-    );
-    const msg = `${platformId}: withdraw: ${outAmount} ${outAddress}: `;
+    txid: string,
+    amount: string
+  } | string> => {
+    const msg = `${platformId}: withdraw: ${outAmount} -> ${outAddress}`;
+    this.log(platform, `${msg}: command received`);
     try {
       const sats = Util.toSats(outAmount);
-      let error: string = '';
       if (!WalletManager.isValidAddress(outAddress)) {
-        error = 'invalid address';
-      } else if (isNaN(outAmount)) {
-        error = 'invalid amount';
+        return `invalid address: \`${outAddress}\``;
       } else if (sats < MIN_OUTPUT_AMOUNT) {
-        error = `withdraw minimum is ${Util.toXPI(MIN_OUTPUT_AMOUNT)} XPI`;
-      }
-      if (error) {
-        throw new Error(error);
+        return `withdraw minimum is ${Util.toXPI(MIN_OUTPUT_AMOUNT)} XPI`;
       }
       const {
         accountId,
@@ -220,18 +190,14 @@ export class Handler extends EventEmitter {
       // Get the user's XAddress and check against outAddress
       const addresses = this.wallets.getXAddresses(accountId);
       if (addresses.includes(outAddress)) {
-        throw new Error('you must withdraw to an external wallet');
+        return `you must withdraw to an external wallet`;
       }
       // Get the user's balance and check against outAmount
       const balance = await this.wallets.getAccountBalance(accountId);
       if (sats > balance) {
-        this.log(
-          platform,
-          msg + `insufficient balance: ${sats} > ${balance}`
-        );
-        throw new Error('insufficient balance');
+        return `insufficient balance: ${sats} > ${balance}`;
       }
-      // Generate transaction and get num of utxos used in the tx
+      // Generate withdrawal tx
       const tx = await this.wallets.genTx({
         fromAccountId: accountId,
         outAddress,
@@ -246,14 +212,14 @@ export class Handler extends EventEmitter {
           userId
         });
       } catch (e: any) {
-        throw new Error('failed to save withdrawal');
+        throw new Error(`failed to save withdrawal: ${e.message}`);
       }
-      this.log(DB, msg + `saved: ${tx.txid}`);
+      this.log(DB, `${msg}: saved: ${tx.txid}`);
       // try to broadcast the withdrawal tx
       try {
         // Broadcast the withdrawal to network
         const txid = await this.wallets.broadcastTx(tx);
-        this.log(WALLET, msg + `accepted: ${txid}`);
+        this.log(WALLET, `${msg}: accepted by network: ${txid}`);
         // Get the actual number of sats in the tx output to reply to user
         const outSats = tx.outputs[0].satoshis;
         return {
@@ -263,12 +229,10 @@ export class Handler extends EventEmitter {
       } catch (e: any) {
         // If tx broadcast fails, delete the withdrawal database entry
         await this.prisma.deleteWithdrawal(tx.txid);
-        throw new Error('withdrawal broadcast failed');
+        throw new Error(`withdrawal broadcast failed: ${e.message}`);
       }
     } catch (e: any) {
-      // Return the error to the platform for notifying the user
-      this.log(platform, `${msg}: ERROR: ${e.message}`);
-      return { error: e.message };
+      throw new Error(`${msg}: ${e.message}`);
     }
   };
 
@@ -276,10 +240,11 @@ export class Handler extends EventEmitter {
     platform: PlatformName,
     platformId: string,
     secret: string | undefined,
-  ) => {
+  ): Promise<{
+    secret: string
+  } | string> => {
     this.log(platform, `${platformId}: link command received`);
-    const msg = `${platformId}: link: ${secret ? '<redacted>' : 'initiate'}: `;
-    let error: string;
+    const msg = `${platformId}: link: ${secret ? '<redacted>' : 'initiate'}`;
     try {
       const { accountId, userId } = await this._getIds(platform, platformId);
       switch (typeof secret) {
@@ -289,18 +254,15 @@ export class Handler extends EventEmitter {
           const linkAccountId = await this.prisma.getAccountIdFromSecret(secret);
           // sanity checks
           if (!linkAccountId) {
-            error = 'invalid secret provided';
+            return 'invalid secret provided';
           } else if (linkAccountId == accountId) {
-            error = 'own secret provided or already linked';
-          }
-          if (error) {
-            throw new Error(error);
+            return 'own secret provided or already linked';
           }
           // try to update the user's accountId
           await this.prisma.updateUserAccountId(userId, linkAccountId);
           this.log(
             platform,
-            msg + `successfully linked to ${linkAccountId} accountId`
+            `${msg}: successfully linked to ${linkAccountId} accountId`
           );
           // update walletkey with new accountId
           this.wallets.updateKey(userId, accountId, linkAccountId);
@@ -316,7 +278,7 @@ export class Handler extends EventEmitter {
       }
     } catch (e: any) {
       this.log(platform, `${msg}: ERROR: ${e.message}`);
-      return { error: e.message };
+      throw new Error(`${msg}: ${e.message}`);
     }
   };
 
@@ -324,15 +286,15 @@ export class Handler extends EventEmitter {
     platform: PlatformName,
     platformId: string,
   ) => {
-    const msg = `${platformId}: backup: `;
-    this.log(platform, msg + `command received`);
+    const msg = `${platformId}: backup`;
+    this.log(platform, `${msg}: command received`);
     try {
       const { userId } = await this._getIds(platform, platformId);
       const mnemonic = await this.prisma.getUserMnemonic(userId);
       return mnemonic;
     } catch (e: any) {
       this.log(platform, `${msg}: ERROR: ${e.message}`);
-      return { error: e.message };
+      throw new Error(`${msg}: ${e.message}`);
     }
   };
   
@@ -412,15 +374,15 @@ export class Handler extends EventEmitter {
         if (typeof user == 'string' || !user) {
           continue;
         }
-        const platform = platformName as PlatformName;
-        const platformId = user.id;
         const { accountId } = deposit.user;
         const balance = await this.wallets.getAccountBalance(accountId);
-        return {
+        return this.emit('DepositSaved', {
+          platform: platformName as PlatformName,
+          platformId: user.id,
           txid: utxo.txid,
           amount: Util.toXPI(utxo.value),
           balance: Util.toXPI(balance)
-        };
+        });
       }
     } catch (e: any) {
       throw new Error(`_saveDeposit: ${e.message}`);
